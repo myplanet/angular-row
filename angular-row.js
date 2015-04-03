2850f5d6793ab6757087685a29054ca8c64e60ed
Ates Goral <ates.g@myplanetdigital.com>
2015-04-03 14:56:11 -0400

    Initial commit of implementation

---
+++ angular-row.js
@@ -0,0 +1,33 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([ 'module', 'angular' ], function (module, angular) {
+            module.exports = factory(angular);
+        });
+    } else if (typeof module === 'object') {
+        module.exports = factory(require('angular'));
+    } else {
+        if (!root.mp) {
+            root.mp = {};
+        }
+
+        root.mp.row = factory(root.angular);
+    }
+}(this, function (angular) {
+    'use strict';
+
+    return angular.module('mp.row', []).directive('angular-row', [ '$window', '$locale', function ($window, $locale) {
+        // Introduce custom elements for IE8
+        $window.document.createElement('row');
+
+        return {
+            restrict: 'AE',
+            replace: true,
+            transclude: true,
+            template: '<div class="row"><div class="_columns" ng-transclude></div></div>',
+
+            link: function (scope, element, attributes) {
+                attributes.$set('columns', element.children().eq(0).children().length);
+            }
+        };
+    }]);
+}));
